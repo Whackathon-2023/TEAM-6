15f285a3372c23832bbf35e8517075ce5e9313cd
Elodin77 <developerdjay@gmail.com>
2023-09-06 18:09:46 +0800

    changed to echo bot

--- bot.js
+++ bot.js
@@ -1,48 +1,31 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 
-let fetch;
-import('node-fetch').then(module => {
-  fetch = module.default;
-});
-
-
 class EchoBot extends ActivityHandler {
-    constructor() {
-        super();
-        this.onMessage(async (context, next) => {
-            const userText = context.activity.text;
-            const replyText = await fetchResponseFromPython(userText);
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
-            await next();
-        });
-
-        this.onMembersAdded(async (context, next) => {
-            const welcomeText = 'yoyo wassup';
-            const membersAdded = context.activity.membersAdded;
-            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
-                }
-            }
-            await next();
-        });
-    }
-}
-
-async function fetchResponseFromPython(userText) {
-    // Send text to Python server
-    await fetch('http://127.0.0.1:5000/send_text', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ content: userText })
+  constructor() {
+    super();
+    // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+    this.onMessage(async (context, next) => {
+      const replyText = `Echo: ${context.activity.text}`;
+      await context.sendActivity(MessageFactory.text(replyText, replyText));
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
     });
 
-    // Fetch the response from Python server
-    const response = await fetch('http://127.0.0.1:5000/get_response');
-    const data = await response.json();
-    return data.response;
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded;
+      const welcomeText = 'Hello and welcome!';
+      for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+        if (membersAdded[cnt].id !== context.activity.recipient.id) {
+          await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+        }
+      }
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
+  }
 }
 
 module.exports.EchoBot = EchoBot;
--- index.js
+++ index.js
@@ -13,9 +13,10 @@ const restify = require('restify');
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
 const {
-    CloudAdapter,
-    ConfigurationServiceClientCredentialFactory,
-    createBotFrameworkAuthenticationFromConfiguration
+  CloudAdapter,
+  ConfigurationServiceClientCredentialFactory,
+  createBotFrameworkAuthenticationFromConfiguration,
+  BotFrameworkAdapter
 } = require('botbuilder');
 
 // This bot's main dialog.
@@ -26,14 +27,19 @@ const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+  console.log(`\n${server.name} listening to ${server.url}`);
+  console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+  console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
 
 const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
-  MicrosoftAppId: '8eb6940c-5400-4830-95fc-0de223520b6a',
+  MicrosoftAppId: "8eb6940c-5400-4830-95fc-0de223520b6a",
+  MicrosoftAppPassword: "",
+  MicrosoftAppType: "",
+  MicrosoftAppTenantId: ""
 });
+
+//lcH8Q~lgPC7SQ9WabrHaLoEr76Js-U9ER-452cuS
 
 const botFrameworkAuthentication = createBotFrameworkAuthenticationFromConfiguration(null, credentialsFactory);
 
@@ -43,22 +49,22 @@ const adapter = new CloudAdapter(botFrameworkAuthentication);
 
 // Catch-all for errors.
 const onTurnErrorHandler = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
-    // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights.
-    console.error(`\n [onTurnError] unhandled error: ${ error }`);
+  // This check writes out errors to console log .vs. app insights.
+  // NOTE: In production environment, you should consider logging this to Azure
+  //       application insights.
+  console.error(`\n [onTurnError] unhandled error: ${error}`);
 
-    // Send a trace activity, which will be displayed in Bot Framework Emulator
-    await context.sendTraceActivity(
-        'OnTurnError Trace',
-        `${ error }`,
-        'https://www.botframework.com/schemas/error',
-        'TurnError'
-    );
+  // Send a trace activity, which will be displayed in Bot Framework Emulator
+  await context.sendTraceActivity(
+    'OnTurnError Trace',
+    `${error}`,
+    'https://www.botframework.com/schemas/error',
+    'TurnError'
+  );
 
-    // Send a message to the user
-    await context.sendActivity('The bot encountered an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+  // Send a message to the user
+  await context.sendActivity('The bot encountered an error or bug.');
+  await context.sendActivity('To continue to run this bot, please fix the bot source code.');
 };
 
 // Set the onTurnError for the singleton CloudAdapter.
@@ -69,17 +75,17 @@ const myBot = new EchoBot();
 
 // Listen for incoming requests.
 server.post('/api/messages', async (req, res) => {
-    // Route received a request to adapter for processing
-    await adapter.process(req, res, (context) => myBot.run(context));
+  // Route received a request to adapter for processing
+  await adapter.process(req, res, (context) => myBot.run(context));
 });
 
 // Listen for Upgrade requests for Streaming.
 server.on('upgrade', async (req, socket, head) => {
-    // Create an adapter scoped to this WebSocket connection to allow storing session data.
-    const streamingAdapter = new CloudAdapter(botFrameworkAuthentication);
+  // Create an adapter scoped to this WebSocket connection to allow storing session data.
+  const streamingAdapter = new CloudAdapter(botFrameworkAuthentication);
 
-    // Set onTurnError for the CloudAdapter created for each connection.
-    streamingAdapter.onTurnError = onTurnErrorHandler;
+  // Set onTurnError for the CloudAdapter created for each connection.
+  streamingAdapter.onTurnError = onTurnErrorHandler;
 
-    await streamingAdapter.process(req, socket, head, (context) => myBot.run(context));
+  await streamingAdapter.process(req, socket, head, (context) => myBot.run(context));
 });
