fb9727cd5ef176c1c19726d38ee089922ec6a99e
joshshipton <shiptonjosh@gmail.com>
2023-09-07 19:02:21 +0800

    rendering hard coded images works

--- teams-bot/bot.js
+++ teams-bot/bot.js
@@ -1,4 +1,7 @@
-const { ActivityHandler, MessageFactory } = require('botbuilder');
+const { ActivityHandler, ActionTypes, ActivityTypes, CardFactory, MessageFactory } = require('botbuilder');
+
+
+// current objective get a random image link sending to the bot
 
 let fetch;
 import('node-fetch').then(module => {
@@ -10,29 +13,52 @@ class ATGENIE extends ActivityHandler {
         super();
         this.onMessage(async (context, next) => {
             const userText = context.activity.text;
-            console.log(`User text is ${userText}`)
-            const replyText = await fetchResponseFromPython(userText);
-            //let replyText = 'I am working.'
-            // if starts with a tab, remove it
-            if (replyText.startsWith('\t')) {
-                replyText = replyText.substring(1);
+            console.log(`User text is ${userText}`);
+
+            const reply = { type: ActivityTypes.Message };
+            reply.text = 'FUCK YEA SENDING PHOTOS WORKS';
+            reply.attachments = [this.getInternetAttachment()];
+
+            const typing = {
+                type: 'typing',
+                from: {id: 'bot-id'},
+                recipient: { id: 'user-id' },
+                conversation: { id: 'conversation-id' }
             }
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            // make the bot start typing
+            // not sure if this will work couldn't find the docs
+            context.sendActivity(typing);
+            // await context.sendActivity(MessageFactory.text(replyText, replyText));
+            await context.sendActivity(reply);
             await next();
         });
 
         this.onMembersAdded(async (context, next) => {
-            const welcomeText = 'Welcome, I am ATGenie. Ask me anything about service tickets';
+            const welcomeText = "## Hey there, I'm service Genie\n# Here are some things I can do:\n- Find similar tickets\n- Give insights into service desk members\n- Retrieve or summarize data from old tickets\n- Send me a message to get started!";
+
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
                 }
-            }
+            }                                                       
             await next();
         });
+
+
+
+    }
+
+    getInternetAttachment() {
+        return {
+            name: 'testing.png',
+            contentType: 'image/png',
+            contentUrl: 'https://cms.hugofox.com//resources/images/a0fea022-8ec7-4a37-b4e7-214846e7656f.jpg'
+        };
     }
 }
+
+
 
 async function fetchResponseFromPython(userText) {
     // Send text to Python server
@@ -47,10 +73,39 @@ async function fetchResponseFromPython(userText) {
 
     // Fetch the response from Python server
     const data = await response.json();
+    if(data.type == 'image'){
+        // do stuff here
+        console.log('Recieved an image going to send an image now')
+        const img = new Image();
+        // img.src = data.image;
+        img.src = 'https://cms.hugofox.com//resources/images/a0fea022-8ec7-4a37-b4e7-214846e7656f.jpg';
+        const formData = new FormData();
+        formData.append('image', img.src);
+        fetch('https://your-bot-url.com/api/messages', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer <your-bot-secret>',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            type: 'message',
+            attachments: [{
+            contentType: 'image/jpeg',
+            contentUrl: img.src,
+            name: 'image.jpg'
+            }]
+        })
+        });
+    }
     console.log(`Recieved data from flask server json data is ${data}`);
-    console.log(`Response to give user is ${data.content}`)
-    // data sometimes comes in weird formats like nested arrays flatten just flattens it out and .join makes it a string
+    console.log(`Text response to give user is ${data.content}`)
     return data.content;
-}
+    
+    }
+
+
+
+
+
 
 module.exports.ATGENIE = ATGENIE;
